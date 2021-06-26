2d5f4133e417be8a9cf143cfcfabe6ed4563c925
Takeshi Yashiro <takeshi.yashiro@iniad.org>
2021-06-26 16:12:43 +0900

    Apply vote count for vote.html

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+kinoko_count = 3
+takenoko_count = 5
+messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
+
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
