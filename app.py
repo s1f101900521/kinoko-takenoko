26d6a7dfb37e06b90b40d738df8db3f4bc94646f
Takeshi Yashiro <takeshi.yashiro@iniad.org>
2021-06-26 18:48:56 +0900

    Use for in template

--- app.py
+++ app.py
@@ -25,14 +25,9 @@ def answer():
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
 
-    message_html = ''
+    messages_data = []
     for i in range(len(messages)):
-        message = messages[i]
-        message = re.sub(r'&', r'&amp;', message)
-        message = re.sub(r'<', r'&lt;', message)
-        message = re.sub(r'<', r'&gt;', message)
-        message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
-            message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+        messages_data.append((messages[i], 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5'))
 
     return render_template('vote.html', **vars())
 
