d7b0cbb4d4e4a7de9ae9a398c37d391502792cbb
Takeshi Yashiro <takeshi.yashiro@iniad.org>
2021-06-26 13:38:12 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def top():
+    return render_template('index.html', **vars())
+
+@app.route('/vote', methods=['POST'])
+def answer():
+    return render_template('vote.html', **vars())
+
+if __name__ == '__main__':
+    app.run(debug=True)
