d81bde28e1bfee1c5900e84fcabb43f6bc694937
Takeshi Yashiro <takeshi.yashiro@iniad.org>
2021-06-26 17:38:05 +0900

    Apply messages to /vote

--- app.py
+++ app.py
@@ -11,14 +11,25 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
-    global kinoko_count, takenoko_count
+    global kinoko_count, takenoko_count, messages
     if request.form.get("item") == 'kinoko':
         kinoko_count += 1
     elif request.form.get("item") == 'takenoko':
         takenoko_count += 1
+
+    messages.append(request.form.get("message"))
+    if len(messages) > 3:
+        messages = messages[-3:]
     
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+
+    message_html = ''
+    for i in range(len(messages)):
+        message = messages[i]
+        message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
+            message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
+
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
