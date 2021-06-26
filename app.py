a45bedc146bc5c4df3d114f8ed829b91998cda1f
Takeshi Yashiro <takeshi.yashiro@iniad.org>
2021-06-26 19:15:43 +0900

    Convert *...* to strong

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def answer():
         message = re.sub(r'&', r'&amp;', message)
         message = re.sub(r'<', r'&lt;', message)
         message = re.sub(r'<', r'&gt;', message)
+        message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
         message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
             message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
 
